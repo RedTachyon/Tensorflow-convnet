19773d6a1a59169288e09cca3273fa8889ad6d6c
RedTachyon <jakub.kwiatkowski.96@gmail.com>
2017-09-20 23:55:31 +0200

    Added Neptune integration

---
+++ neptune/graph_construction.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+
+def classifier(learning_rate=1e-3, use_dropout=True):
+    """
+    Builds a computation graph.
+    
+    Args:
+        learning_rate: float, learning rate to be used in the training algorithm
+        use_dropout: boolean, whether to use dropout while training
+        
+    Returns:
+        model: initialization op
+        train_op: training op
+        accuracy: accuracy op
+        X: placeholder for the features
+        y: placeholder for the labels
+        prob: placeholder for dropout keep_prob value
+    """
+    tf.reset_default_graph()
+    X = tf.placeholder(dtype=tf.float32, shape=[None, 32, 32, 3])
+    y = tf.placeholder(dtype=tf.float32, shape=[None, 1])
+    prob = tf.placeholder_with_default(input=tf.constant(1.), shape=[])
+
+    labels = tf.one_hot(tf.cast(y, tf.int32), depth=10)
+    labels = tf.squeeze(labels, axis=1)
+
+
+    W_conv1 = tf.get_variable(name='Wconv1', shape=[5, 5, 3, 32], dtype=tf.float32,
+                              initializer=tf.contrib.layers.xavier_initializer_conv2d())
+    b_conv1 = tf.get_variable(name='bconv1', shape=[32], dtype=tf.float32, initializer=tf.zeros_initializer)
+
+    h_conv1 = tf.nn.relu(tf.nn.conv2d(X, W_conv1, strides=[1,1,1,1], padding='SAME') + b_conv1)
+    h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    if use_dropout:
+        h_pool1 = tf.nn.dropout(h_pool1, prob)
+
+
+    W_conv2 = tf.get_variable(name='Wconv2', shape=[5, 5, 32, 32], dtype=tf.float32,
+                             initializer=tf.contrib.layers.xavier_initializer_conv2d())
+    b_conv2 = tf.get_variable(name='bconv2', shape=[32], dtype=tf.float32, initializer=tf.zeros_initializer)
+
+    h_conv2 = tf.nn.relu(tf.nn.conv2d(h_pool1, W_conv2, strides=[1,1,1,1], padding='SAME') + b_conv2)
+    h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+    if use_dropout:
+        h_pool2 = tf.nn.dropout(h_pool2, prob)
+
+
+    W_fc1 = tf.get_variable(name='Wfc1', shape=[8*8*32, 256], dtype=tf.float32, 
+                            initializer=tf.contrib.layers.xavier_initializer())
+    b_fc1 = tf.get_variable(name='bfc1', shape=[256], dtype=tf.float32, initializer=tf.zeros_initializer)
+
+    h_pool2_flat = tf.reshape(h_pool2, shape=[-1, 8*8*32])
+
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    if use_dropout:
+        h_fc1 = tf.nn.dropout(h_fc1, prob)
+
+
+    W_fc2 = tf.get_variable(name='Wfc2', shape=[256, 10], dtype=tf.float32, 
+                            initializer=tf.contrib.layers.xavier_initializer())
+    b_fc2 = tf.get_variable(name='bfc2', shape=[10], dtype=tf.float32, initializer=tf.zeros_initializer)
+
+    z = tf.matmul(h_fc1, W_fc2) + b_fc2
+
+    y_hat = tf.nn.softmax(z)
+
+
+
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=z))
+
+    correct_prediction = tf.equal(tf.argmax(labels,1), tf.argmax(y_hat,1))
+
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+
+    model = tf.global_variables_initializer()
+
+
+    return model, train_op, accuracy, X, y, prob
---
+++ neptune/main.py
@@ -0,0 +1,112 @@
+#!/bin/python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+
+from data_preprocessing import *
+from graph_construction import *
+
+
+
+def train_validation_split(data, labels, n_valid):
+    """
+    Splits the data to a training and validation sets.
+    """
+    valid_data, train_data = data[:n_valid], data[n_valid:]
+    valid_labels, train_labels = labels[:n_valid], labels[n_valid:]
+    
+    return train_data, train_labels, valid_data, valid_labels
+
+def load_all_data(path_train, path_test, n_valid=1000):
+    """
+    Loads train and test data from the given paths, normalizes them properly.
+    """
+    data, mean, std, labels = extract_and_norm_data(path_train)
+    test_data, test_labels = extract_test_data(path_test, mean, std)
+    
+    train_data, train_labels, valid_data, valid_labels = train_validation_split(data, labels, n_valid)
+    return train_data, train_labels, valid_data, valid_labels, test_data, test_labels
+
+def get_batch(data, labels, step, size):
+    """
+    Get a minibatch of the given size, maintaining the appripriate order.
+    
+    Args:
+        data: np.array, contains features, first dimension corresponds to samples
+        labels: np.array, contains labels, first dimension corresponds to samples
+        step: int, step of the training
+        size: int, size of the minibatch
+        
+    returns:
+        two arrays with shape (size, (...)), containing the data and labels minibatch
+    """
+    # Rename variables for mathematical elegance
+    m = data.shape[0]
+    b = size
+    i = step
+    
+    low = int(b * (i % np.ceil(m/b)))
+    high = int(b * (i % np.ceil(m/b)) + b)
+    return data[low:high], labels[low:high]
+
+def get_big_accuracy(data, labels, accuracy, sess, X, y, batch_size=100):
+    """
+    Computes the accuracy over the data in case it's too large to simply run the accuracy op on it.
+    """
+    m = data.shape[0]
+    full_score = np.array([])
+    for step in tqdm(np.arange(np.ceil(m/batch_size)), disable=True):
+        eval_data, eval_labels = get_batch(data, labels, step, batch_size)
+        acc = sess.run(accuracy, feed_dict={X: eval_data, y: eval_labels})
+        full_score = np.append(full_score, acc * eval_data.shape[0])
+    return np.sum(full_score) / m
+
+def train_model(num_epochs, batch_size, learning_rate):
+    """
+    Trains the convnet model and measures its accuracy on the train, validation and test sets.
+    """
+    train_data, train_labels, valid_data, valid_labels, test_data, test_labels = load_all_data('../train_32x32.mat',
+                                                                                               '../test_32x32.mat')
+    model, train_op, accuracy, X, y, prob = classifier(1e-3, True)
+    
+    num_iters = int(np.ceil(train_data.shape[0] / batch_size) * num_epochs)
+    
+    with tf.Session() as sess:
+        print("Training the model")
+        sess.run(model)
+        accs = np.array([])
+        val_accs = np.array([])
+        
+        for step in tqdm(range(num_iters), disable=True):
+            data_batch, label_batch = get_batch(train_data, train_labels, step, batch_size)
+            _, acc = sess.run([train_op, accuracy], feed_dict={X: data_batch, y: label_batch, prob: .8})
+            accs = np.append(accs, acc)
+            if not step % 10:
+                val_acc = sess.run(accuracy, feed_dict={X: valid_data, y: valid_labels})
+                val_accs = np.append(val_accs, val_acc)
+                print("val_acc " + str(val_acc))
+            print("accuracy " + str(acc))
+        
+        plt.subplot(211)
+        plt.plot(range(accs.shape[0]), accs)
+        plt.subplot(212)
+        plt.plot(range(val_accs.shape[0]), val_accs)
+        
+        print("Evaluating the training set")
+        train_acc = get_big_accuracy(train_data, train_labels, accuracy, sess, X, y)
+        print("Evaluating the validation set")
+        val_acc = get_big_accuracy(valid_data, valid_labels, accuracy, sess, X, y)
+        print("Evaluating the test set")
+        test_acc = get_big_accuracy(test_data, test_labels, accuracy, sess, X, y)
+        
+        print("Training accuracy: %.3f" % train_acc)
+        print("Validation accuracy: %.3f" % val_acc)
+        print("Test accuract: %.3f" % test_acc)
+        
+        plt.show()
+        
+        
+if __name__ == '__main__':
+    train_model(5, 512, 1e-3)
